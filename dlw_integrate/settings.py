e96031896cdd9d4ec8b447487bd86c88f0d0aa6a
madhu179 <madhubabu232343@gmail.com>
2019-08-05 15:50:35 +0530

    madhu

--- dlw_integrate/settings.py
+++ dlw_integrate/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dlw_new',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
